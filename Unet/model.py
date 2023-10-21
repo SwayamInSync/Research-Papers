f02178f1e07e58600b66f0c12008e9f22fa864ef
Swayam <74960567+SwayamInSync@users.noreply.github.com>
2023-10-21 09:01:38 +0530

    Update Unet to use UpSample and CenterCrop

--- Unet/model.py
+++ Unet/model.py
@@ -1,77 +1,84 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms.functional as TF
 
 
-class DoubleConv(nn.Module):
+class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
+        super(UNet, self).__init__()
 
-    def forward(self, x):
-        return self.conv(x)
+        # Encoder
+        self.enc1 = self.conv_block(in_channels, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
 
-
-class UNET(nn.Module):
-    def __init__(
-            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
-    ):
-        super(UNET, self).__init__()
-        self.ups = nn.ModuleList()
-        self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # Down part of UNET
-        for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature))
-            in_channels = feature
+        # Decoder
+        self.up3 = self.up_conv(512, 256)
+        self.dec3 = self.conv_block(512, 256)
+        self.up2 = self.up_conv(256, 128)
+        self.dec2 = self.conv_block(256, 128)
+        self.up1 = self.up_conv(128, 64)
+        self.dec1 = self.conv_block(128, 64)
 
-        # Up part of UNET
-        for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(
-                    feature*2, feature, kernel_size=2, stride=2,
-                )
-            )
-            self.ups.append(DoubleConv(feature*2, feature))
-
-        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
-        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
 
     def forward(self, x):
-        skip_connections = []
+        # Encoder
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool(e1))
+        e3 = self.enc3(self.pool(e2))
+        e4 = self.enc4(self.pool(e3))
 
-        for down in self.downs:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pool(x)
+        # Decoder
+        d3 = self.up3(e4)
+        e3 = self.center_crop(e3, d3.size()[2], d3.size()[3])
+        d3 = torch.cat((e3, d3), dim=1)
+        d3 = self.dec3(d3)
 
-        x = self.bottleneck(x)
-        skip_connections = skip_connections[::-1]
+        d2 = self.up2(d3)
+        e2 = self.center_crop(e2, d2.size()[2], d2.size()[3])
+        d2 = torch.cat((e2, d2), dim=1)
+        d2 = self.dec2(d2)
 
-        for idx in range(0, len(self.ups), 2):
-            x = self.ups[idx](x)
-            skip_connection = skip_connections[idx//2]
+        d1 = self.up1(d2)
+        e1 = self.center_crop(e1, d1.size()[2], d1.size()[3])
+        d1 = torch.cat((e1, d1), dim=1)
+        d1 = self.dec1(d1)
 
-            if x.shape != skip_connection.shape:
-                x = TF.resize(x, size=skip_connection.shape[2:])
+        out = self.out(d1)
+        return out
 
-            concat_skip = torch.cat((skip_connection, x), dim=1)
-            x = self.ups[idx+1](concat_skip)
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
 
-        return self.final_conv(x)
+    def up_conv(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        )
+
+    def center_crop(self, layer, target_width, target_height):
+        _, _, layer_height, layer_width = layer.size()
+        delta_width = layer_width - target_width
+        delta_height = layer_height - target_height
+        top = delta_height // 2
+        bottom = top + target_height
+        left = delta_width // 2
+        right = left + target_width
+        return layer[:, :, top:bottom, left:right]
 
 
 if __name__ == "__main__":
-    x = torch.randn((3, 1, 160, 160))
-    model = UNET(in_channels=1, out_channels=1)
+    x = torch.randn((3, 1, 161, 161))
+    model = UNet(in_channels=1, out_channels=1)
     preds = model(x)
+    print(preds.shape)
     assert preds.shape == x.shape
     print("success")
